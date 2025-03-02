cec9ba1e1ecd1df93643ff9ad82c644ba83fbce6
Kackar212 <kackar212@gmail.com>
2025-03-02 22:34:35 +0100

    feat: integrate ThemeSwitch and useTheme to allow theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeSwitch } from "./components/theme-switch/theme-switch.component";
+import { twMerge } from "tailwind-merge";
+import { useTheme } from "./hooks/use-theme.hook";
+
 function App() {
-  return null;
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className={twMerge("size-full bg-background", theme)}>
+      <ThemeSwitch setTheme={setTheme} theme={theme} />
+    </div>
+  );
 }
 
 export default App;
