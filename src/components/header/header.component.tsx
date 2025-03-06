8e89e8bb7eda258c3574d89282eb100352b4c24d
Kackar212 <kackar212@gmail.com>
2025-03-06 20:59:11 +0100

    style: increase max width of header

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -5,7 +5,7 @@ import { Experience } from "../experience/experience.component";
 
 export function Header() {
   return (
-    <header className="flex flex-col w-full px-4 justify-center md:px-8 lg:px-12 pt-20 pb-16 leading-none max-w-6xl m-auto">
+    <header className="flex flex-col w-full px-4 justify-center md:px-8 lg:px-12 pt-20 pb-16 leading-none max-w-7xl m-auto">
       <Nav />
       <div className="flex flex-col items-center mt-8 gap-4 md:items-stretch md:flex-row lg:gap-12 xl:gap-36">
         <HeaderCard />
