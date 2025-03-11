0c87f5342d558234e2012633262ed9897bacac59
Kackar212 <kackar212@gmail.com>
2025-03-11 20:40:04 +0100

    chore: install and configure emailjs

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import emailjs from "@emailjs/browser";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n.ts";
+
+emailjs.init({
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_API_KEY,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
