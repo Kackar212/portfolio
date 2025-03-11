d66b2247f6f32fa44e7f0e104bf6c8f8fcd077cd
Kackar212 <kackar212@gmail.com>
2025-03-11 18:20:23 +0100

    style: refine form field structure and appearance

--- src/components/form-field/form-field.component.tsx
+++ src/components/form-field/form-field.component.tsx
@@ -14,7 +14,7 @@ export function FormField({ render, name, label }: FormFieldProps) {
   const { t } = useTranslation(["contact"]);
   const inputProps = {
     className:
-      "text-primary border-0 focus-visible:ring-0 p-0 h-[19px] text-sm resize-none px-1.5 font-light break-words",
+      "text-primary border-0 focus-visible:ring-0 p-0 h-[19px] text-sm resize-none px-1.5 font-light break-words shadow-none",
     name,
   };
 
@@ -23,13 +23,15 @@ export function FormField({ render, name, label }: FormFieldProps) {
       name={name}
       render={({ field }) => (
         <Form.FormItem>
-          <div className="ring-primary/15 rounded-md pt-6 px-4 pb-1 relative w-full focus-within:ring-2 has-[[data-error=true]]:ring-destructive-foreground dark:bg-neutral-950/60 bg-soft-gray">
-            <Form.FormLabel className="px-1.5 mt-1 absolute top-1 text-sm uppercase leading-none w-[calc(100%-var(--spacing)*4)] data-[error=true]:text-primary font-bold">
+          <div className="flex flex-col">
+            <Form.FormLabel className="px-1.5 my-1 text-sm uppercase leading-none w-[calc(100%-var(--spacing)*4)] data-[error=true]:text-primary font-bold">
               {t(label)}
             </Form.FormLabel>
-            <Form.FormControl>
-              {render({ ...inputProps, ...field })}
-            </Form.FormControl>
+            <div className="ring-primary/15 rounded-md p-2 relative w-full focus-within:ring-2 has-[[data-error=true]]:ring-destructive-foreground dark:bg-neutral-950/60 bg-soft-gray">
+              <Form.FormControl>
+                {render({ ...inputProps, ...field })}
+              </Form.FormControl>
+            </div>
             <Form.FormMessage className="mt-2 px-1.5" />
           </div>
         </Form.FormItem>
