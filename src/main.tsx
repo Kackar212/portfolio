eac02db00687b7639057b8c84b8f6eee16ebb8e5
Kackar212 <kackar212@gmail.com>
2025-03-20 17:09:19 +0100

    fix: dynamically set lang attribute based on user language

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import emailjs from "@emailjs/browser";
 import App from "./App.tsx";
 import "./index.css";
-import "./i18n.ts";
+import i18n from "./i18n.ts";
 
 emailjs.init({
   publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_API_KEY,
 });
+
+document.documentElement.lang = i18n.language;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
